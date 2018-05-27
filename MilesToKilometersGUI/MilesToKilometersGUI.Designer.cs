d55ce0e413bb851de30a2f3222a9c63ff0878c0a
Harley Ehrman <harley.ehrman@outlook.com>
2018-05-26 20:43:32 -0400

    Add project files.

---
+++ MilesToKilometersGUI/MilesToKilometersGUI.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace MilesToKilometersGUI
+{
+    partial class MilesToKilometersGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblFactor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(229, 125);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(167, 56);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(429, 381);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(204, 57);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit Application";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(67, 78);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(257, 25);
+            this.lblInput.TabIndex = 2;
+            this.lblInput.Text = "Enter the number of miles";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(330, 75);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(66, 31);
+            this.txtInput.TabIndex = 3;
+            // 
+            // lblFactor
+            // 
+            this.lblFactor.AutoSize = true;
+            this.lblFactor.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFactor.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblFactor.Location = new System.Drawing.Point(73, 217);
+            this.lblFactor.Name = "lblFactor";
+            this.lblFactor.Size = new System.Drawing.Size(0, 25);
+            this.lblFactor.TabIndex = 4;
+            // 
+            // MilesToKilometersGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 450);
+            this.Controls.Add(this.lblFactor);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "MilesToKilometersGUI";
+            this.Text = "MilesToKilometersGUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblFactor;
+    }
+}
+
---
+++ MilesToKilometersGUI/MilesToKilometersGUI.cs
@@ -0,0 +1,59 @@
+﻿/********************************************************************
+ * Assignment from SDEV 240                                         *
+ * Coded by: Harley Ehrman                                          *
+ *                                                                  *
+ * From book:                                                       *
+ * Microsoft Visual C# 2015:                                        *
+ * An Introduction to Object-Oriented Programming 6th Edition       *
+ * by Joyce Farrell                                                 *
+ *                                                                  *
+ * Instructions (Page 134, Exercise 1):                             *
+ * Write a GUI program named MilesToKilometersGUI that allows the   *
+ * user to input a distance in miles and output the value in        *
+ * kilometers. There are 1.6 kilometers in a mile.                  *
+ ********************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MilesToKilometersGUI
+{
+    public partial class MilesToKilometersGUI : Form
+    {
+        public MilesToKilometersGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //define variables
+            const Double KilosPerMile = 1.6;
+            double NumMiles, Factor;
+
+            //set if to validate for numbers only in input
+            if (double.TryParse(txtInput.Text, out NumMiles))
+            {
+                NumMiles = Convert.ToDouble(txtInput.Text);
+                Factor = NumMiles * KilosPerMile;
+                lblFactor.Text = NumMiles + " mile(s) is equal to " + Factor + " kilometer(s).";
+            }
+            else
+            {
+                lblFactor.Text = "ERROR! You must enter a number.";
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
